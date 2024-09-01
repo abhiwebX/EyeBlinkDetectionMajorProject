f32955c77433375fc312d1cec07761784f2cb23f
Abhijeet Dhekane <119113356+abhiwebX@users.noreply.github.com>
2024-09-01 20:04:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,28 +1,45 @@
-import numpy as np
-import cv2 as cv2
-try:
-    import dlib
-    print("Dlib is installed.")
-    print("Dlib version:", dlib.__version__)
-except ImportError:
-    print("Dlib is not installed.")
-
-cap = cv2.VideoCapture(0)
-
-detector = dlib.get_frontal_face_detector()
-
+import cv2
+import mediapipe as mp
+import pyautogui as pyi
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w , screen_h = pyi.size()
 while True:
-    _, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    _, frame = cam.read()
+    frame = cv2.flip(frame,1)
+    rgb_face = cv2.cvtColor(frame,cv2.COLOR_HSV2BGR)
+    output = face_mesh.process(rgb_face)
+    lankmarks = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
+    
+    if lankmarks:
+        lank = lankmarks[0].landmark
+        for id,l in enumerate(lank[474:478]):
+            x = int( l.x * frame_w)
+            y = int (l.y * frame_h)
+            cv2.circle(frame,(x,y),3,(0,255,0))
+            if id == 1:
+                screen_x = screen_w / frame_w * x
+                screen_y = screen_h / frame_h * y
+              #  pyi.moveTo(screen_x,screen_y)
+        left = [lank[145],lank[159]]
+        for l in left:
+            x = int( l.x * frame_w)
+            y = int (l.y * frame_h)
+            cv2.circle(frame,(x,y),3,(0,255,255))
+        if(left[0].y-left[1].y) < 0.004:
+            print('Eye Is blinked')
+            pyi.click()
+            pyi.sleep(1)
+            
 
-    faces = detector(gray)
-    for face in faces:
-       print(face)
 
-    cv2.imshow("Frame",frame)
+
+
+    cv2.imshow("Eye Controlled Mouse",frame)
     key = cv2.waitKey(1)
     if key == 27:
         break
-cap.release()
+    
+cam.release()
 cv2.destroyAllWindows()
-
